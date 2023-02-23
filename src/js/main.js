899cb04e6e7d452aa1680ce15b2874c3410a1012
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-23 19:56:21 +0100

    add fetch and variables in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('>> Ready :)');
+//*
+//!
+//?
+//
+//todo
+
+//* Global variables - cogemos elementos del HTML
+// const inputElement = document.querySelector('.js-input');
+// const searchBtn = document.querySelector('.js-searchBtn');
+// const resetBtn = document.querySelector('.js-resetBtn');
+// const cocktailsList = document.querySelector('.js-cocktails');
+// const favoritesList = document.querySelector('.js.favorites');
+const margaritasUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+let listMargaritasData = [];
+
+//? When the user is loading the page the list of Margaritas should appear
+fetch(margaritasUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data); // to see which property I have to chose
+    listMargaritasData = data.drinks.map((showDrink) => ({
+      name: showDrink.strDrink,
+      photo: showDrink.strDrinkThumb,
+    }));
+    console.log(listMargaritasData);
+  });
+
+
