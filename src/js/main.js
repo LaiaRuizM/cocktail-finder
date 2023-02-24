47869bbd3db9bf0df4520cfa0e8fcd5b1a1da62b
LaiaRuizM <laia.ruizm@hotmail.com>
2023-02-24 18:05:52 +0100

    add favorite list

--- src/js/main.js
+++ src/js/main.js
@@ -11,25 +11,25 @@ const inputElement = document.querySelector('.js-input');
 const searchBtn = document.querySelector('.js-searchBtn');
 // const resetBtn = document.querySelector('.js-resetBtn');
 const cocktailsList = document.querySelector('.js-cocktailsUl');
-//const favoritesList = document.querySelector('.js.favorites'); //ul favs
-const margaritasUrl =
+const favoritesList = document.querySelector('.js-favoritesUl'); //ul favs
+const url =
   `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`;
-let listMargaritasData = [];
-let listAllDrinks = []; // el resto de cócteles (drinks/coctéles)
-let listArrayCocktail = [];  // es el array para recoger el listado de las favoritas: favoritesList -> listado para meter los cócteles
-// let listArrayFavorite = []; listado para meter los favoritos
+let cocktailsListData = []; 
+let favoritesListArray = [];  // es el array para recoger el listado de las favoritas: favoritesList -> listado para meter los cócteles listArrayFavorite
+// let listArrayFavorite = []; listado para meter los favoritos -> ELIMINAR
 
 //? When the user is loading the page the list of Margaritas should appear
-fetch(margaritasUrl)
+fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data); // to see which property I have to chose
-    listMargaritasData = data.drinks.map((showDrink) => ({
+    console.log(data); // to see which property I have to chose (drinks)
+    cocktailsListData = data.drinks.map((showDrink) => ({
       name: showDrink.strDrink,
       photo: showDrink.strDrinkThumb,
       id: showDrink.idDrink,
     }));
-    renderCocktails(listMargaritasData); //donde pintarlo -> cambiarlo por el render
+    renderCocktails(cocktailsListData); //donde pintarlo -> cambiarlo por el render
+    //renderFavoritesList(favoritesList);
   });
 
 //? function to paint the list of delicious cocktails
@@ -51,28 +51,42 @@ function handleClickSearch(ev) {
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputUserCocktail}`)
     .then((response) => response.json())
     .then((data) => {
-      listAllDrinks = data.drinks.map((showDrink) => ({
+      cocktailsListData = data.drinks.map((showDrink) => ({
         name: showDrink.strDrink,
         photo: showDrink.strDrinkThumb,
         id: showDrink.idDrink,
       }));
       cocktailsList.innerHTML = ''; //
-      renderCocktails(listAllDrinks); //donde pintarlo -> cambiarlo por el render -> OK
+      renderCocktails(cocktailsListData); //donde pintarlo -> cambiarlo por el render -> OK
+      renderFavoritesList(favoritesList);
     });
 }
 
 //* Function to add cocktails to favorite list
 function handleClickList(ev) {
   console.log(ev.currentTarget.id);
-  ev.currentTarget.classList.toggle('selected');
-  const idSelected = listArrayCocktail.ev.currentTarget.id; 
-
+  const idSelected = ev.currentTarget.id;
+  const cocktailSelected = cocktailsListData.find(cocktailItem => cocktailItem.id === idSelected); 
+  //console.log(cocktailSelected);
+  const cocktailIndex = favoritesListArray.findIndex(cocktailItem => cocktailItem.id === idSelected); // el index es la posición
+  console.log(cocktailIndex);
+  if (cocktailIndex === -1) {  // Compruebo si existe el favorito cocktailIndex, si no existe en favoritos (-1), pues lo añado (push) en favoritos -> ADD
+    ev.currentTarget.classList.add('selected');
+    favoritesListArray.push(cocktailSelected);
+  } else {
+    ev.currentTarget.classList.remove('selected'); // Compruebo si existe el favorito, si existe en favoritos (i), pues lo elimino (splice) de favoritos -> REMOVE
+    favoritesListArray.splice(cocktailIndex, 1); // splice = elimina un elemento a partir de una posición y con el 1 indicamos que no borre todo, que se elimine él, si es 2 es él mismo + next y así.
+  }
+  renderFavoritesList(favoritesListArray);
 }
 
-
-
-
-
+function renderFavoritesList(drinkFav) {
+  favoritesList.innerHTML = '';
+  console.log('holis');
+  for (const eachDrinkFav of drinkFav) {
+    favoritesList.innerHTML += `<li class="js-liDrink" id="${eachDrinkFav.id}"><h4>${eachDrinkFav.name}</h4><img src="${eachDrinkFav.photo}" title="${eachDrinkFav.name}" alt="${eachDrinkFav.name}" class="cocktailImg"/></li>`;
+  }
+}
 
 
 //* Events
